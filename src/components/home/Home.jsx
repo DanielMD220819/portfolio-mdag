9588f285a01f061e31d96729da1d71d10f00da19
DanielMD220819 <daniel261112180814@gmail.com>
2024-04-23 02:20:41 -0600

    Segundo commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './home.css'
+import Me from '../../assets/avatar-1.svg'
+import HeaderSocials from './HeaderSocials'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className='home container' id='home'>
+            <div className="intro">
+                <img src={Me} alt="me" className="home__img" />
+                <h1 className="home__name">Monserrat DAG</h1>
+                <span className="home__education">I'm a Front-End developer</span>
+                <HeaderSocials />
+            </div>
+        </section>
     )
 }
 
