95a44a54835856eaa0e5328de31f673b30529caf
DanielMD220819 <daniel261112180814@gmail.com>
2024-04-27 01:10:50 -0600

    Cuarto commit

--- src/components/potfolio/Portfolio.jsx
+++ src/components/potfolio/Portfolio.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Menu from './Menu'
 import './portfolio.css'
 
 const Portfolio = () => {
+    const [items, setItems] = useState(Menu);
+    const filterItem = (categoryItem) => {
+        const updateItem = Menu.filter((curElem) => {
+            return curElem.category === categoryItem;
+        })
+        setItems(updateItem);
+    }
     return (
-        <div>Portfolio</div>
+        <section className="work container section" id='work'>
+            <h2 className="section__title">Recent works</h2>
+            <div className="work__filters">
+                <span className="work__item" onClick={() => setItems(Menu)}>Everything</span>
+                <span className="work__item" onClick={() => filterItem("Creative")}>Creative</span>
+                <span className="work__item" onClick={() => filterItem("Art")}>Art</span>
+                <span className="work__item" onClick={() => filterItem("Design")}>Design</span>
+                <span className="work__item" onClick={() => filterItem("Branding")}>Branding</span>
+            </div>
+            <div className="work__container grid">
+                {items.map((elem) => {
+                    const { id, image, title, category } = elem;
+                    return (
+                        <div className="work__card" key={id}>
+                            <div className="work__thumbnail">
+                                <img src={image} alt={title} className='work__img' />
+                                <div className="work__mask">
+
+                                </div>
+                            </div>
+                            <span className="work__category">{category}</span>
+                            <h3 className="work__title">{title}</h3>
+                            <a href="#c" className="work__button">
+                                <i className='icon-link work__button-icon'></i>
+                            </a>
+                        </div>
+                    )
+                })}
+            </div>
+        </section>
     )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './testimonials.css'
+import Image1 from '../../assets/avatar-1.svg'
+import Image3 from '../../assets/avatar-3.svg'
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "John Doe",
+        subtitle: "Product designer at Dribble",
+        comment:
+            "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
+    },
+    {
+        id: 2,
+        image: Image3,
+        title: "John Doe",
+        subtitle: "Product designer at Dribble",
+        comment:
+            "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
+    }
+]
 
 const Testimonials = () => {
     return (
-        <div>Testimonials</div>
+        <section className="testimonials container section">
+            <h2 className="section__title">Clients & reviews</h2>
+            <Swiper className="testimonials__container grid" modules={[Pagination]} spaceBetween={30} slidesPerView={1} loop={true} grabCursor={true} pagination={{ clickable: true }}>
+                {data.map(({ id, image, title, subtitle, comment }) => {
+                    return (
+                        <SwiperSlide className="testimonials__item" key={id}>
+                            <div className="thumb">
+                                <img src={image} alt="" className="testimonials__img" />
+                            </div>
+                            <h3 className="testimonials__title">{title}</h3>
+                            <span className="testimonials__subtitle">{subtitle}</span>
+                            <p className="testimonials__comment">{comment}</p>
+                        </SwiperSlide>
+                    )
+                })}
+            </Swiper>
+        </section>
     )
 }
 
